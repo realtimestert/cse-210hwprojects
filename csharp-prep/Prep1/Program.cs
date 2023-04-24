e352872f9fc5ea332b36256b01a2bc98e1167e3b
stuart <istewpotter1@gmail.com>
2023-04-24 14:37:10 -0400

    Index Modified

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
         
-        Console.Write("What is your last name ? ");
+        Console.Write("What is your last name? ");
         String last = Console.ReadLine();
         
         Console.WriteLine($"Your name is {last}, {first} {last}.");
