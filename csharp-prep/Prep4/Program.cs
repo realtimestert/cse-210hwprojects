0b5e5766b7da2fcf4c6b1afb7d5eaecd81aadde0
stuart <istewpotter1@gmail.com>
2023-04-25 10:11:07 -0400

    more changes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,21 +5,17 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-        List<string> words = new List<string>();
-        words.Add("phone");
-        words.Add("keyboard");
-        words.Add("mouse");
-
-        Console.WriteLine($"There are {words.Count} words in the list.");
-
-        foreach (string word in words)
+        
+        int userNumber = -1;
+        while (userNumber != 0)
         {
-            Console.WriteLine(word);
-        }
+            Console.Write("Enter a number (0 to quit): ");
+            userNumber = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < words.Count; i++)
-        {
-            Console.WriteLine(words[i]);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
         }
     }
     
