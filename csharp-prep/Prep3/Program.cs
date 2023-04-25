6a18c13f2ada9862bb5d4656ddac300f3b082e85
stuart <istewpotter1@gmail.com>
2023-04-24 20:51:01 -0400

    prep 2 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        string response;
+
+        do
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+
+        for (int i = 2; i < 20; i = i+2)
+        {
+            Console.WriteLine(i);
+        }
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
     }
 }
