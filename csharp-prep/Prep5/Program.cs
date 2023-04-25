f0aadcc36971a34609253e717818704a13e50f7b
stuart <istewpotter1@gmail.com>
2023-04-25 12:12:21 -0400

    made it work

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,16 +5,18 @@ class Program
     static void Main(string[] args)
     {
         DisplayWelcome();
-        PromtUserName();
-        PromptUserNumber();
-        SquareNumber(int number);
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
 
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
         }       
 
-        static string PromtUserName()
+        static string PromptUserName()
         {
             Console.Write("What is your name? ");
             string userName = Console.ReadLine();
@@ -28,7 +30,6 @@ class Program
             number = int.Parse(Console.ReadLine());
             return number;
         }
-
 
         static int SquareNumber(int number)
         {
