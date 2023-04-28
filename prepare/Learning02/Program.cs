8b77cfbcd9b92a6b0a89396739cc612c80ca8a0a
stuart <istewpotter1@gmail.com>
2023-04-27 21:16:18 -0400

    edited jobs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,16 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._company = "Red Hat";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = "2005";
-        job1._endYear = "2017";
+        job1._company = "School District";
+        job1._jobTitle = "School Counselor";
+        job1._startYear = "2018";
+        job1._endYear = "2022";
         //job1.DisplayJobDetails();
 
         Job job2 = new Job();
         job2._company = "Valve";
         job2._jobTitle = "Decorative Plant";
-        job2._startYear = "2017";
+        job2._startYear = "2022";
         //job2.DisplayJobDetails();
 
         Resume myResume = new Resume();
