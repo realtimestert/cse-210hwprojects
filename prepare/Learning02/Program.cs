114d85c3083ed6d8526003ccc21eab198d66ab7d
stuart <istewpotter1@gmail.com>
2023-04-27 21:14:49 -0400

    added resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,14 +8,21 @@ class Program
         job1._company = "Red Hat";
         job1._jobTitle = "Software Engineer";
         job1._startYear = "2005";
-        job1._endYear = "2022";
-        job1.DisplayJobDetails();
+        job1._endYear = "2017";
+        //job1.DisplayJobDetails();
 
         Job job2 = new Job();
         job2._company = "Valve";
         job2._jobTitle = "Decorative Plant";
         job2._startYear = "2017";
-        job2.DisplayJobDetails();
-    }
+        //job2.DisplayJobDetails();
 
+        Resume myResume = new Resume();
+        myResume._name = "Stuart Evans";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }
 }
