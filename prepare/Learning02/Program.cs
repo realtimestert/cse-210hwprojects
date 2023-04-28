c5be697e035cf46fa89c91989279ee4d442b10a7
stuart <istewpotter1@gmail.com>
2023-04-27 20:50:33 -0400

    initial commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Red Hat";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2005";
+        job1._endYear = "2023";
     }
+
 }
