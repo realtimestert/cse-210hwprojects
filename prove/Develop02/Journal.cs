eadedba4ca458316a85d03c74721cdf6c1d7064d
stuart <istewpotter1@gmail.com>
2023-04-29 22:24:01 -0400

    lots of changes

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,10 +1,57 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
 class Journal
 {
-    public List<Entry> Entries;
-    public PromptGenerator PromptGenerator;
-    public Journal(PromptGenerator promptGenerator)
-    {
-        Entries = new List<Entry>();
-        PromptGenerator = promptGenerator;
-    }
+    static void Main(string[] args){}
+        public List<Entry> Entries;
+        public PromptGenerator PromptGenerator;
+        public Journal(PromptGenerator promptGenerator)
+        {
+            Entries = new List<Entry>();
+            PromptGenerator = promptGenerator;
+        }
+
+        public void DisplayJournalEntries()
+        {
+            int count = 0;
+            if (Entries.Count ==0)
+            {
+                Console.WriteLine("No entries in the journal.");
+            }
+
+            else
+            {
+                foreach(Entry entry in Entries)
+                {
+                    Console.WriteLine($"Entry #{++count}:");
+                    entry.DisplayEntry();
+                }
+                Console.WriteLine($"Total entries: {Entries.Count}\n");
+            }
+        }
+        
+        public void CreateJournalEntry()
+        {
+            DateTime currentDate = DateTime.Now.Date;
+            string prompt = PromptGenerator.ChoosePrompt();
+
+            Console.WriteLine($"{prompt}");
+            string response = Console.ReadLine();
+
+            Entry entry = new Entry(currentDate.ToString("MM/dd/yyyy"), prompt, response);
+            Entries.Add(entry);
+
+            Console.WriteLine("Entry added to journal.");
+        }
+
+        public interface SaveToCsv()
+        {
+            if (Entries.Count == 0)
+            {
+                Console.WriteLine("Nothing to save");
+                return 0;
+            }
+        }
 }
