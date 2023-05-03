fa7d3a0b4fa9daa7f50face6c5604b23b6ba19a8
stuart <istewpotter1@gmail.com>
2023-05-02 21:06:50 -0400

    Finished Program

--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -10,5 +10,33 @@ public class Fraction
         _top = 1;
         _bottom = 1;
     }
-}
+
+    public Fraction(int wholeNumber)
+    {
+
+        //Console.WriteLine("Please give a whole number: ");
+        //int top = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine(top); 
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
     
+}   
