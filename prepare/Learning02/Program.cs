1357a8ecc63c72be127af9aee0f4ba086ab4c2ce
stuart <istewpotter1@gmail.com>
2023-05-09 21:12:41 -0400

    more learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,10 +12,16 @@ class Program
         //job1.DisplayJobDetails();
 
         Job job2 = new Job();
-        job2._company = "ACTION Inc.";
-        job2._jobTitle = "Case Manager";
+        job2._company = "Certified Clean Care";
+        job2._jobTitle = "Carpet Technician";
         job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._endYear = 2022;
+
+        Job job3 = new Job();
+        job3._company = "ACTION Inc.";
+        job3._jobTitle = "Case Manager";
+        job3._startYear = 2022;
+        job3._endYear = 2023;
         //job2.DisplayJobDetails();
 
         Resume myResume = new Resume();
@@ -23,6 +29,7 @@ class Program
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
 
         myResume.Display();
     }
