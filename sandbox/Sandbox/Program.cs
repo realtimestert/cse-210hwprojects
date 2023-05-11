395dedff151ce84bdb26757bbb358324cb461df6
stuart <istewpotter1@gmail.com>
2023-05-10 23:40:34 -0400

    reverted changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,15 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello there!");
-        
-        //This is a constructor
-        Person p = new Person();
-        p.SetFirstName("Peter");
-
-        Console.WriteLine(p.GetFirstName());
-
-        Person p1 = new Person();
-        Person p2 = new Person("Stuart", "Evans");
-        Person p3 = new Person("Mr.", "Stuart", "Evans");
     }
 }
