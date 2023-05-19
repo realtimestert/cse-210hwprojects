5211f2b0b4ec25d5af2d4ae5338c1134fe95a43a
stuart <istewpotter1@gmail.com>
2023-05-18 21:25:44 -0400

    initial commit

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,16 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filename = "myFile.txt";
-    string[] lines = System.IO.File.ReadAllLines(filename);
+        Console.WriteLine("Welcome to the Goal Setting Program!");
 
-    foreach (string line in lines)
-    {
-        string[] parts = line.Split(",");
-
-        string firstName = parts[0];
-        string lastName = parts[1];
-    }
-        Console.WriteLine("Hello Develop05 World!");
+        //string choice = " ";
     }
 }
---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool goalComplete;
+
+    public SimpleGoal()
+    {
+        goalComplete = false;
+    }
+
+    //Pass the information along
+    public SimpleGoal(string)
+}
