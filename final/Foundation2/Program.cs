a9d2b13b0ff5878b904df9cc484f8053a53d480a
stuart <istewpotter1@gmail.com>
2023-05-30 20:26:43 -0400

    initial commit

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,22 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Product product1 = new Product("Rusty Shovel", "542609", 19.99m, 1);
-        Product product2 = new Product("Toothbrush", "3478", 3.49m, 9);
-        Product product3 = new Product("Haunted Claymore", "634570", 99.99m, 1);
+        Product product1 = new Product("Rocket Launcher", "542609", 2.49m, 18);
+        Product product2 = new Product("Righteous Bison", "3478", 157.98m, 1);
+        Product product3 = new Product("Shovel", "634570", 99.99m, 1);
 
-        Address address1 = new Address("123 Gravel Lane", "Teufort", "New Mexico", "USA");
-        Customer customer1 = new Customer("Jane Doe", address1);
+        Address address = new Address("123 Gravel Lane", "Teufort", "New Mexico", "USA");
+        Customer customer = new Customer("Jane Doe", address);
+        
         List<Product> products = new List<Product>();
         products.Add(product1);
         products.Add(product2);
-        Order order = new Order(customer1, products);
+        Order order = new Order(customer, products);
 
+        /*
         Address address2 = new Address("756 Smissmas Drive", "Edinburg", "Scotland", "Great Britain");
         Customer customer2 = new Customer("Tavish DeGroot", address2);
         List<Product> productB = new List<Product>();
         products.Add(product3);
         Order order2 = new Order(customer2, productB);
+        */
 
         Console.WriteLine("Packing Label: ");
         Console.WriteLine(order.CreatePackingLabel());
